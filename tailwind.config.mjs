5ef04e5683c9d144b459beaa464a16baf75d3893
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-05-20 13:15:27 +0530

    Base Animation is working

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('daisyui'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        slideUp: {
+          "0%": { transform: "translateY(0px)" },
+          "100%": { transform: "translateY(100px)"},
+        },
+      },
+      animation: {
+        slideUp: "slideUp 1s ease-out",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
