e7b314e61d581030d93a0e7c772109c5b1d3de81
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-05-20 14:46:15 +0530

    Hi

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -6,12 +8,12 @@ export default {
       keyframes: {
         slideUp: {
           "0%": { transform: "translateY(0px)"},
-          "50%": { borderRadius: "10%", height: "200px" },
+          "50%": { transform: 'translateY(-15dvh)', borderRadius: "50%", height: "200px", width: '200px' },
           "100%": {
             transform: "translateY(-45dvh)",
             borderRadius: "50%",
-            width: "100px",
-            height: "100px",
+            width: "10px",
+            height: "10px",
           },
         },
 		
