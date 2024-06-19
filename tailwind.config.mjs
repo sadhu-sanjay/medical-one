b5f519eb8992b421af344c6385b2646479bd7623
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-06-19 16:18:35 +0530

    updated color scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,4 @@
-import { transform } from 'typescript';
+import { transform } from "typescript";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,8 +7,13 @@ export default {
     extend: {
       keyframes: {
         slideUp: {
-          "0%": { transform: "translateY(0px)"},
-          "50%": { transform: 'translateY(-15dvh)', borderRadius: "50%", height: "200px", width: '200px' },
+          "0%": { transform: "translateY(0px)" },
+          "50%": {
+            transform: "translateY(-15dvh)",
+            borderRadius: "50%",
+            height: "200px",
+            width: "200px",
+          },
           "100%": {
             transform: "translateY(-45dvh)",
             borderRadius: "50%",
@@ -16,10 +21,17 @@ export default {
             height: "10px",
           },
         },
-		
       },
       animation: {
         slideUp: "slideUp 1.5s ease-in-out",
+      },
+      colors: {
+        primary: `var(rgb(--color-primary))`,
+        secondary: `var(rgb(--color-secondary))`,
+        
+        background: `var(rgb(--color-background))`,
+        surface: `var(rgb(--color-surface))`,
+        text: `var(rgb(--color-text))`,
       },
     },
   },
